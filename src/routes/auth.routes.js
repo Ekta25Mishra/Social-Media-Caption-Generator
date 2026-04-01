6e57f78b25d480eb6bf2ff606a180c5248b29fe1
Ekta25Mishra <mishrae3104@gmail.com>
2026-04-01 20:06:37 +0530

    bcryptjs introduced

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,14 @@
 const express= require("express")
-
 const router= express.Router()
+const {registerController,loginController}= require("../controllers/auth.controller")
+
 
 /* POST /register
 POST /login 
 GET /user [protected] */
+
+router.post('/register',registerController)
+router.post('/login',loginController)
+
 
 module.exports= router
